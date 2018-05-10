a30b763252b35c95de250c1bd76f56a283b437ff
Maggie Baber <mbbaber@gmail.com>
2018-05-10 10:12:48 +0200

    basic structure html css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MainComponent } from './main/main.component';
+import { FlashcardComponent } from './flashcard/flashcard.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
-  //main component = user's main page with list of subjects
+  //main component = user's main page with list of subjects (groups is in drop down menu)
   {path: 'main', component: MainComponent},
   //flashcard page
-  //{path: '/phone/:blahId', component: },
-  
+  {path: 'flashcard', component: FlashcardComponent},
+
   //Error Component
   //{path: '**', component: },
-
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MainComponent } from './main/main.component';
+import { FlashcardComponent } from './flashcard/flashcard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    MainComponent
+    MainComponent,
+    FlashcardComponent
   ],
   imports: [
     BrowserModule,
